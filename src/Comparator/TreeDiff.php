94ddb189fc48cbab3ad899916c750d888a5fe8e2
alexk <alexk@imac-alexk.local>
2016-02-03 23:05:17 +0100

    clean code

--- src/Comparator/TreeDiff.php
+++ src/Comparator/TreeDiff.php
@@ -9,42 +9,51 @@
 namespace JsonDiff\Comparator;
 
 use JsonDiff\ValueObject\TreeInterface;
+use JsonDiff\ValueObject\Tree as TreeObject;
 
 class TreeDiff implements DiffInterface
 {
     public function diff(TreeInterface $first, TreeInterface $second)
     {
-        $ret=\JsonDiff\ValueObject\Tree::fromArray([]);
+        $ret = TreeObject::fromArray([]);
 
         foreach ($second->getKeys() as $key) {
 
-            $value=$second->getKey($key);
+            $value = $second->getKey($key);
 
             if (!$first->keyExists($key)) {
-                $ret->setValue($key,$value);
+                $ret->setValue($key, $value);
+
             } else if ($value instanceof TreeInterface) {
 
-                $firstValue=$first->getKey($key);
+                $subTree = $first->getKey($key);
+                $this->compareSubTree($subTree, $value, $ret, $key);
 
-                if ($firstValue instanceof TreeInterface) {
-
-                    if($value->getHash() != $firstValue->getHash()) {
-                        $ret->setValue($key,$this->diff($firstValue,$value));
-                    }
-
-                } else {
-
-                    $ret->setValue($key,$value);
-
-                }
             } else if ($first->getKey($key) != $value) {
 
-                $ret->setValue($key,$value);
+                $ret->setValue($key, $value);
 
             }
         }
 
         return $ret;
+    }
+
+    /**
+     * @param $subTree
+     * @param $value
+     * @param $ret
+     * @param $key
+     */
+    private function compareSubTree($subTree, $value, &$ret, $key)
+    {
+        if ($subTree instanceof TreeInterface) {
+            if ($value->getHash() != $subTree->getHash()) {
+                $ret->setValue($key, $this->diff($subTree, $value));
+            }
+        } else {
+            $ret->setValue($key, $value);
+        }
     }
 
 }
